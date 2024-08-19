6a9582dcc8a446518cbed7b18efe7349879be193
DiegoBarrosA <diegobarrosaraya@outlook.com>
2024-08-19 18:10:48 -0400

    first commit

---
+++ src/main/java/com/diegobarros/springmovies/SpringmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.diegobarros.springmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegobarros/springmovies/SpringmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegobarros.springmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
